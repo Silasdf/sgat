f8128607cc4fce3a513ea791aa7cac93170ba50b
Silas da Silva Fonseca <silas@localhost.localdomain>
2017-03-02 17:19:12 -0300

    Alteração no segundo commit

--- src/sgat/janelas/JanelaCadastroCliente.java
+++ src/sgat/janelas/JanelaCadastroCliente.java
@@ -3,10 +3,11 @@ package sgat.janelas;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+//import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class JanelaCadastroCliente extends Application {
@@ -16,13 +17,15 @@ public class JanelaCadastroCliente extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
 		// Cria um botão e inclui o clique
 		Button btn_incluir = new Button("Incluir");
+		Button btn_limpar = new Button("Limpar");
+		Button btn_pesquisar = new Button("Pesquisar");
 		Button btn_sair = new Button("Sair");
-		btn_sair.setOnAction(e -> System.exit(0));
+		
 		btn_incluir.setOnAction(new EventHandler<ActionEvent>(){
 			
-			@Override
 			// Inclui mensagem no console
 			public void handle(ActionEvent event){
 				System.out.println("Incluir cadastro cliente");
@@ -30,37 +33,37 @@ public class JanelaCadastroCliente extends Application {
 			}
 		});
 		
-		VBox root = new VBox();
-		root.getChildren().addAll(btn_incluir, btn_sair);
+		btn_limpar.setOnAction(new EventHandler<ActionEvent>(){
+			
+			// Limpar mensagem no console
+			public void handle(ActionEvent event){
+				System.out.println("Limpar cadastro cliente");
+				
+			}
+		});
+		
+		btn_pesquisar.setOnAction(new EventHandler<ActionEvent>(){
+			
+			// Pesquisar mensagem no console
+			public void handle(ActionEvent event){
+				System.out.println("Pesquisar cadastro cliente");
+				
+			}
+		});
+		
+		btn_sair.setOnAction(e -> {
+			System.out.println("Sair da aplicação");
+			System.exit(0);
+		});
+		
+		HBox root = new HBox();
+		root.setSpacing(10.0);
+		root.setAlignment(Pos.BOTTOM_RIGHT);
+		root.getChildren().addAll(btn_incluir, btn_limpar, btn_pesquisar, btn_sair);
 		Scene scene = new Scene(root,600,400);
 		primaryStage.setTitle("Cadastro - Clientes");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-//		try {
-//			BorderPane root = new BorderPane();
-//			//Criação do botão
-//			Button b = new Button("Bem Vindo JavaFX");
-////			b.setOnAction(new EventHandler<ActionEvent>(){
-////			
-////				//Ação do botão, clique
-////				public void handle(ActionEvent event){
-////					System.out.println("Bem vindo via setOnAction!");
-////				}
-////			});
-//			b.addEventHandler(ActionEvent.ACTION, new EventHandler<ActionEvent>(){
-//				
-//				//Ação do botão, clique outra forma
-//				public void handle(ActionEvent event){
-//					System.out.println("Bem vindo via addEventHandler!");
-//				}
-//			});
-//			root.setCenter(b);
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-//		} catch(Exception e) {
-//			e.printStackTrace();
-//		}
+
 	}
 }
