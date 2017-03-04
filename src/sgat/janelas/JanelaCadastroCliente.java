d0150d2eb28fc9987d603b7480a269f2fae96d08
Silas da Silva Fonseca <silas@localhost.localdomain>
2017-03-04 19:20:46 -0300

    Inclusão do textfield e labels

--- src/sgat/janelas/JanelaCadastroCliente.java
+++ src/sgat/janelas/JanelaCadastroCliente.java
@@ -6,14 +6,17 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-//import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class JanelaCadastroCliente extends Application {
+	
 	public static void main(String[] args){
 		launch(args);
-	}
+	} // Fim do método main
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -23,6 +26,11 @@ public class JanelaCadastroCliente extends Application {
 		Button btn_limpar = new Button("Limpar");
 		Button btn_pesquisar = new Button("Pesquisar");
 		Button btn_sair = new Button("Sair");
+		
+		Label lblNome = new Label("Nome:");
+		TextField tfNome = new TextField();
+		Label lblCpf = new Label("CPF:");
+		TextField tfCpf = new TextField();
 		
 		btn_incluir.setOnAction(new EventHandler<ActionEvent>(){
 			
@@ -56,14 +64,25 @@ public class JanelaCadastroCliente extends Application {
 			System.exit(0);
 		});
 		
-		HBox root = new HBox();
-		root.setSpacing(10.0);
-		root.setAlignment(Pos.BOTTOM_RIGHT);
-		root.getChildren().addAll(btn_incluir, btn_limpar, btn_pesquisar, btn_sair);
-		Scene scene = new Scene(root,600,400);
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(12);
+		
+		HBox hbButtons = new HBox();
+		hbButtons.setSpacing(10.0);
+		hbButtons.setAlignment(Pos.BOTTOM_RIGHT);
+		
+		hbButtons.getChildren().addAll(btn_incluir, btn_limpar, btn_pesquisar, btn_sair);
+		grid.add(lblNome, 0, 0);
+		grid.add(tfNome, 1, 0);
+		grid.add(lblCpf, 0, 1);
+		grid.add(tfCpf, 1, 1);
+		Scene scene = new Scene(grid,600,400); // Aparecer os labels e Textfields
+		//Scene scene1 = new Scene(hbButtons,600,400); // Aparecer os botões
 		primaryStage.setTitle("Cadastro - Clientes");
-		primaryStage.setScene(scene);
+		primaryStage.setScene(scene); // Aparecer os labels e Textfields
+		//primaryStage.setScene(scene1); // Aparecer os botões
 		primaryStage.show();
 
-	}
-}
+	}//Fim do método Start
+}// Fim da classe JanelaCadastroCliente
